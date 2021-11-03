e09d074516148ed54000242bab5369412c384434
Derek Poston <Hamersgaven01@gmail.com>
2021-11-03 13:33:37 -0500

    first update

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,10 +17,47 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    if (isNaN(testInput)){
+        return "Not a Number"
+    }
+    if(typeof(testInput) === "number"){
+        return "Is a Number"
+    }
+    if(typeof(testInput) === "string"){
+        return "Empty"
+    }
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if(validateInput(pilot) !== "Empty" || validateInput(copilot) !== "Empty"){
+        window.alert("Invalid input detected! Please try again!")
+    } else {document.getElementByID("pilotStatus").innerHTML=`Pilot ${pilot} is ready for launch!`;
+            document.getElementByID("copilotStatus").innerHTML=`Co-Pilot ${copilot} is ready for launch!`;
+    }
+    if(validateInput(fuelLevel) !== "Is a Number" || validateInput(cargoLevel !== "Is a Number")){
+        window.alert("Please check fields for valid entries!")
+    }else {
+        if(fuelLevel < 10000){
+            document.getElementByID("faultyItems").style.visibility="visible";
+            document.getElementByID("fuelStatus").innerHTML="Fuel level insufficient";
+            document.getElementByID("launchStatus").style.color="red";
+            document.getElementByID("launchStatus").innerHTML="Shuttle not ready for launch";
+        } else {
+            document.getElementByID("launchStatus").style.color="green";
+            document.getElementByID("launchStatus").innerHTML="Shuttle is ready for launch";
+        }
+        if(cargoLevel > 10000){
+            document.getElementByID("faultyItems").style.visibility="visible";
+            document.getElementByID("cargoStatus").innerHTML="Cargo too heavy!";
+            document.getElementByID("launchStatus").style.color="red";
+            document.getElementByID("launchStatus").innerHTML="Shuttle not ready for launch";
+        }else {
+            document.getElementByID("launchStatus").style.color="green";
+            document.getElementByID("launchStatus").innerHTML="Shuttle is ready for launch";
+        }
+
+    }
    
 }
 
